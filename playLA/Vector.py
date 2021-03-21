75f872ced28bb412d11b67674099a2a2b47b1125
冯金荣 <15201250134@163.com>
2021-03-21 17:47:59 +0800

    向量基本操作

--- playLA/Vector.py
+++ playLA/Vector.py
@@ -5,6 +5,28 @@ class Vector:
     def __getitem__(self, index):
         return self._values[index]
 
+    @classmethod
+    def  zero(cls,dim):
+        return cls([0]*dim)
+
+    def __add__(self, other):
+        """向量加法，返回结果向量"""
+        assert len(self)==len(other),\
+        "Error in adding.Length of vectors must be same"
+        """啥意思？"""
+        return Vector([a+b for a,b in zip(self,other)])
+
+    def __sub__(self, other):
+        assert len(self)==len(other),"Error in adding.Length of vectors must be same"
+        return Vector([a-b for a,b in zip(self,other)])
+
+    def __mul__(self, k):
+        return Vector([k*e for e in self])
+
+    def __rmul__(self, k):
+        return self*k
+
+
     def __len__(self):
         """返回向量的维度(有多少个元素)"""
         return len(self._values)
--- testLA/main_math.py
+++ testLA/main_math.py
@@ -1,7 +1,23 @@
 from playLA.Vector import Vector
 
 if __name__ == '__main__':
-    u=Vector([5,2])
-    print(u)
-    print(len(u))
-    print("vec[0]={},vec[1]={}".format(u[0],u[1]))
+    vec=Vector([5,2])
+    print(vec)
+    print(len(vec))
+    print("vec[0]={},vec[1]={}".format(vec[0],vec[1]))
+    vec2=Vector([3,1])
+    print("{}+{}={}".format(vec, vec2,vec+vec2))
+    xy=zip([5,3],[1,2])
+    # print(list(xy))
+    # for a,b in xy:
+    #     print(a,b)
+    #     print(a+b)
+    zero2=Vector.zero(2)
+    print(zero2)
+    print("{}+{}={}".format(vec, zero2, vec + zero2))
+
+
+
+
+
+
